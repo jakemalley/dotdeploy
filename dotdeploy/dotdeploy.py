ec7433d67ca4805b8f13a9ad7d6e3a9a46b49756
Jake Malley <jja.malley@gmail.com>
2020-01-14 23:29:37 +0000

    Added change reporting and conditional deploy.

--- dotdeploy/dotdeploy.py
+++ dotdeploy/dotdeploy.py
@@ -4,12 +4,52 @@ import os
 import sys
 import copy
 import shutil
+import filecmp
 import argparse
 import configparser
 
 
 class DotDeployException(Exception):
     """DotDeployException"""
+
+
+class Helpers:
+    """
+    namespace for helper functions to keep dotdeploy in a single
+    file for portability, while maintaining organisation.
+    """
+
+    @staticmethod
+    def get_abspath(base_path, *paths):
+        """
+        return the absolute path for a base path,
+        joined with a number of additional paths
+        """
+        return os.path.abspath(os.path.join(base_path, *paths))
+
+    @staticmethod
+    def get_expanded_abspath(base_path, path):
+        """
+        return a absolute path to a given file,
+        while expanding ~ and ~user constructions
+        """
+        # return path if already absolute
+        if os.path.isabs(path):
+            return path
+        # expand user path and check if absolute
+        path = os.path.expanduser(path)
+        if os.path.isabs(path):
+            return path
+        # return a absolute path of base and path
+        return Helpers.get_abspath(base_path, path)
+
+    @staticmethod
+    def is_abspath_prefixed_by(expected_base, path):
+        """
+        return whether a path begins with a given base,
+        e.g. is /abc/def/ghi under the directory /abc
+        """
+        return path.startswith(expected_base)
 
 
 class DotDeploy:
@@ -26,7 +66,7 @@ class DotDeploy:
         self._config = {}
 
         # initialise an argument parser
-        self._parser = argparse.ArgumentParser()
+        self._parser = argparse.ArgumentParser(prog=self.NAME)
         self._parser._positionals.title = "command"
         subparsers = self._parser.add_subparsers(dest="command")
 
@@ -54,11 +94,22 @@ class DotDeploy:
         # apply command
         apply_command = subparsers.add_parser("apply", help="apply a given profile")
         apply_command.add_argument("profile", help="path to a profile.ini file")
+        apply_command.add_argument(
+            "--no-report",
+            action="store_true",
+            help="do not report the number of files changed",
+        )
+        apply_command.add_argument(
+            "--dry-run",
+            action="store_true",
+            help="report without applying file changes",
+        )
 
     def cli(self):
         """
         begin CLI - parse the arguments and run the required function
         """
+        # parse command line arguments
         self._args = self._parser.parse_args()
         if not self._args.command:
             self.show_help()
@@ -68,6 +119,9 @@ class DotDeploy:
             getattr(self, "cmd_{}".format(self._args.command))()
         else:
             self.error("command {} not implemented".format(self._args.command))
+
+        # exit(0)
+        sys.exit(0)
 
     def show_help(self):
         """
@@ -131,6 +185,17 @@ class DotDeploy:
         else:
             return self._config
 
+    def get_group_names(self):
+        """
+        return a list of groups to deploy (excludes settings and *.settings)
+        """
+        blacklisted_sections = ["DEFAULT", "global", "settings"]
+        return [
+            k
+            for k in self._config
+            if k not in blacklisted_sections and not k.endswith(".settings")
+        ]
+
     def validate(self, quiet=True):
         """
         validate a given profile
@@ -153,7 +218,7 @@ class DotDeploy:
             )
             return False
 
-        groups = self.groups(self._config)
+        groups = self.get_group_names()
         if not groups:
             validate_error("no groups found in profile")
             return False
@@ -162,9 +227,9 @@ class DotDeploy:
         valid = True
 
         # for each group ensure it exists, and then check the files
-        for group in self.groups(self._config):
+        for group in self.get_group_names():
 
-            group_path = os.path.join(self._config["global"]["base_path"], group)
+            group_path = Helpers.get_abspath(self._config["global"]["base_path"], group)
             if not os.path.isdir(group_path):
                 valid = False
                 validate_error(
@@ -190,8 +255,8 @@ class DotDeploy:
                     group, file_name, deploy_path
                 )
 
-                if not self.is_abspath_prefixed_by(
-                    os.path.join(self._config["global"]["base_path"], group),
+                if not Helpers.is_abspath_prefixed_by(
+                    Helpers.get_abspath(self._config["global"]["base_path"], group),
                     abs_file_name,
                 ):
                     valid = False
@@ -215,59 +280,61 @@ class DotDeploy:
 
         return valid
 
-    def groups(self, config):
-        """
-        return a list of groups to deploy (excludes settings and *.settings)
-        """
-        blacklisted_sections = ["DEFAULT", "global", "settings"]
-        return [
-            k
-            for k in config
-            if k not in blacklisted_sections and not k.endswith(".settings")
-        ]
-
     def get_paths(self, group, file_name, deploy_path):
         """
         return the absolute file paths to the file and deployment
         location for files within a group
         """
 
-        abs_file_name = os.path.abspath(
-            os.path.join(self._config["global"]["base_path"], group, file_name)
+        abs_file_name = Helpers.get_abspath(
+            self._config["global"]["base_path"], group, file_name
         )
 
-        abs_deploy_path = self.get_expanded_abspath(
+        abs_deploy_path = Helpers.get_expanded_abspath(
             os.path.dirname(abs_file_name), deploy_path
         )
 
         return abs_file_name, abs_deploy_path
 
-    def get_expanded_abspath(self, base_path, path):
+    def file_changed(self, mode, abs_file_path, abs_deploy_path):
         """
-        return a absolute path to a given file expanding user
+        return whether the deployed file should or be updated
+        (to prevent us updating the same file with each run)
         """
-        # return path if already absolute
-        if os.path.isabs(path):
-            return path
-        # expand user path and check if absolute
-        path = os.path.expanduser(path)
-        if os.path.isabs(path):
-            return path
-        # return a absolute path of base and path
-        return os.path.abspath(os.path.join(base_path, path))
+        # if the file doesn't already exist it will need
+        # to be updated.
+        if not os.path.isfile(abs_deploy_path):
+            return True
 
-    def is_abspath_prefixed_by(self, expected_base, path):
-        """
-        return whether a path begins with a given base,
-        e.g. is /abc/def/ghi under the directory /abc
-        """
-        return path.startswith(expected_base)
+        # if mode=link and it currently is a link to
+        # the correct file we don't update
+        if (
+            mode == "link"
+            and os.path.islink(abs_deploy_path)
+            and os.path.samefile(abs_file_path, abs_deploy_path)
+        ):
+            return False
 
-    def backup(self, config, path):
+        # if mode=copy and it is not a link and
+        # they are the same we don't update
+        if (
+            mode == "copy"
+            and not os.path.islink(abs_deploy_path)
+            and filecmp.cmp(abs_file_path, abs_deploy_path)
+        ):
+            return False
+
+        # we need to update the file
+        return True
+
+    def backup(self, path):
         """
         backup a given file
         """
-        backup_base_path = os.path.join(config["global"]["base_path"], "backup")
+
+        backup_base_path = Helpers.get_abspath(
+            self._config["global"]["base_path"], "backup"
+        )
         if not os.path.isdir(backup_base_path):
             os.mkdir(backup_base_path)
 
@@ -287,7 +354,14 @@ class DotDeploy:
         if not self.validate(quiet=False):
             sys.exit(1)
 
-        for group in self.groups(config):
+        # reporting
+        report = (
+            not self._args.no_report or self._args.dry_run or (self._args.verbose > 0)
+        )
+
+        report_data = {"resources": 0, "changed": 0, "backed_up": 0}
+
+        for group in self.get_group_names():
             # update the settings with the group specific settings
             settings = copy.deepcopy(config["settings"])
             settings.update(config.get("{}.settings".format(group), {}))
@@ -295,25 +369,51 @@ class DotDeploy:
             files = config[group]
             # iterate over the files and deploy them
             for file_name, deploy_path in files.items():
+                report_data["resources"] += 1
 
+                # get the absolute paths to the file and file to deploy
                 abs_file_name, abs_deploy_path = self.get_paths(
                     group, file_name, deploy_path
                 )
 
-                print("=" * 80)
-                print("OLD:", abs_file_name)
-                print("NEW:", abs_deploy_path)
-                print("=" * 80)
+                # get mode, default to 'link'
+                mode = settings.get("mode", "link").lower()
+                # has the file changed
+                changed = self.file_changed(mode, abs_file_name, abs_deploy_path)
 
-                # if settings.get("backup", "false").lower() in ["true", "yes"]:
-                #     self.backup(config, abs_deploy_path)
+                if changed:
+                    report_data["changed"] += 1
 
-                # if settings.get("mode", "link").lower() in ["copy", "cp"]:
-                #     shutil.copyfile(abs_file_name, abs_deploy_path)
-                # else:
-                #     if os.path.isfile(abs_deploy_path):
-                #         os.remove(abs_deploy_path)
-                #     os.symlink(abs_file_name, abs_deploy_path)
+                    # if the file has changed take a backup.
+                    if settings.get("backup", "false").lower() in ["true", "yes"]:
+                        report_data["backed_up"] += 1
+                        if not self._args.dry_run:
+                            self.backup(abs_deploy_path)
+
+                    if not self._args.dry_run:
+                        # if we are changing and the file exists remove it.
+                        if os.path.isfile(abs_deploy_path):
+                            os.remove(abs_deploy_path)
+
+                        # if we are copying use shutil.copyfile
+                        if settings.get("mode", "link").lower() in ["copy", "cp"]:
+                            shutil.copyfile(abs_file_name, abs_deploy_path)
+                        # otherwise use os.symlink
+                        else:
+                            os.symlink(abs_file_name, abs_deploy_path)
+
+        if report:
+            report_text = (
+                "file(s) would change" if self._args.dry_run else "files(s) changed"
+            )
+            print(
+                "{} / {} {}, {} file(s) backed up".format(
+                    report_data["changed"],
+                    report_data["resources"],
+                    report_text,
+                    report_data["backed_up"],
+                )
+            )
 
     def cmd_validate(self):
         """
@@ -323,7 +423,7 @@ class DotDeploy:
         quiet = self._args.quiet and (self._args.verbose == 0)
 
         # load the profile and validate it
-        config = self.load_profile(self._args.profile)
+        self.load_profile(self._args.profile)
         valid = self.validate(quiet=quiet)
 
         # if the profile wasn't valid we exit(1)
